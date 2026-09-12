d85a4f3055f39e9d50d3e60db3c0bd150fc49ec2
ALVARO OCIEL SANCHEZ MAGNE <lpze.alvaroociel.sanchez.ma@unifranz.edu.bo>
2026-09-12 12:37:01 -0400

    DTO del usuario actualizado

--- src/main/java/com/unifranz/programaciontres/application/dto/UsuarioDto.java
+++ src/main/java/com/unifranz/programaciontres/application/dto/UsuarioDto.java
@@ -1,7 +1,6 @@
 package com.unifranz.programaciontres.application.dto;
 
 import com.unifranz.programaciontres.domain.Usuario;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,27 +10,28 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class UsuarioDto {
+
     private Long id;
     private String nombre;
     private String email;
+    private String rol;
+
     private LocalDateTime fechaCreacion;
     private LocalDateTime fechaModificacion;
     private String creadoPor;
     private String modificadoPor;
     private Boolean eliminado;
-    private  String rol;
 
     public UsuarioDto(Usuario usuario) {
         this.id = usuario.getId();
         this.nombre = usuario.getNombre();
         this.email = usuario.getEmail();
+        this.rol = usuario.getRol();
         this.fechaCreacion = usuario.getFechaCreacion();
         this.fechaModificacion = usuario.getFechaModificacion();
         this.creadoPor = usuario.getCreadoPor();
         this.modificadoPor = usuario.getModificadoPor();
         this.eliminado = usuario.getEliminado();
-        this.rol = usuario.getRol();
     }
 }
